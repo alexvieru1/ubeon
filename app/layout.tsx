7c9e3e689f8f93bdab9b27e39dd5e4a1a1e0dbe6
Alexandru Vieru <vieru.alexandruu@gmail.com>
2025-03-12 17:48:35 +0200

    no dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <body className="overflow-x-hidden">
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
