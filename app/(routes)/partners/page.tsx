93f3edaf36b5519963d0d5dc207e569da68321d7
Alexandru Vieru <vieru.alexandruu@gmail.com>
2025-04-15 18:17:57 +0300

    some logo fixes

--- app/(routes)/partners/page.tsx
+++ app/(routes)/partners/page.tsx
@@ -9,37 +9,42 @@ const page = () => {
     {
       title: "Med Impact",
       src: "/images/medimpact.png",
-      description:
-        '"Pentru noi UBEON reprezinta un partener de mare valoare. Discret, prompt si profesionist, amabil si deferent. O afacere construita cu mare atentie si grija fata de clienti. Intr-o permanenta cautare de solutii, intr-o permanenta autodepasire profesionala si personala, profesionistii UBEON stiu sa munceasca si sa construiasca o echipa, astfel incat sa avem certitudinea celor mai bune solutii si servicii oferite. Suntem la un apel distanta nu este doar un slogan, este chiar esenta si definitia profesionistilor UBEON!"',
+      imgWidth: 110,
+      imgHeight: 80,
+      description: `"Pentru noi UBEON reprezintă un partener de mare valoare. Discret, prompt și profesionist, amabil și deferent. O afacere construită cu mare atenție și grijă față de clienți. Într-o permanentă căutare de soluții, într-o permanentă autodepășire profesională și personală, profesioniștii UBEON știu să muncească și să construiască o echipă, astfel încât să avem certitudinea celor mai bune soluții și servicii oferite. 'Suntem la un apel distanță' nu este doar un slogan, este chiar esența și definiția profesioniștilor UBEON!"`,
       author: "Iulia Crull - Administrator",
     },
     {
-      title: "Federatia Patronilor Bihor",
+      title: "Federația Patronilor Bihor",
       src: "/images/federatia.png",
-      description:
-        '"UBEON a respectat in totalitate termenele de livrare, procesul de facturare s-a realizat conform cerintelor noastre...UBEON a aratat in permanenta ca trateaza clientii cu profesionalism si este competent sa participe la licitatiile publice, fiind un partener de incredere."',
-      author: "Ioan Lucian - Presedinte",
+      imgWidth: 252,
+      imgHeight: 242,
+      description: `"UBEON a respectat în totalitate termenele de livrare, procesul de facturare s-a realizat conform cerințelor noastre...UBEON a arătat în permanență că tratează clienții cu profesionalism și este competent să participe la licitațiile publice, fiind un partener de încredere."`,
+      author: "Ioan Lucian - Președinte",
     },
     {
       title: "RoMarketing",
       src: "/images/romarketing.png",
-      description:
-        '....Chiar daca selectia a fost facuta pe baza criteriului "pretul cel mai mic", am fost multumiti de alegerea facuta. Echipa UBEON a raspuns promt la toate solicitarile noastre si s-a dovedit un partener de incredere. Recomandam tuturor companiilor sa ia in considerare cu seriozitate oferta UBEON.',
+      imgWidth: 250,
+      imgHeight: 50,
+      description: `"Chiar dacă selecția a fost făcută pe baza criteriului 'prețul cel mai mic', am fost mulțumiți de alegerea făcută. Echipa UBEON a răspuns prompt la toate solicitările noastre și s-a dovedit un partener de încredere. Recomandăm tuturor companiilor să ia în considerare cu seriozitate oferta UBEON."`,
       author: "Anamaria Goina - Responsabil Proiect",
     },
     {
       title: "Star Sting",
       src: "/images/star-sting.png",
-      description:
-        '"Aparitia UBEON a fost pentru noi surprinzatoare. UBEON este partenerul de care aveam nevoie si care in timp a dovedit implicare si atentie la nevoile partenerului. Echipa de acolo este consecventa ideii de a aduce solutii inovatoare in deplin acord cu interesele partenerilor sai. Recomand cu sinceritate sa incepeti o colaborare cu echipa UBEON si sa va convingeti ca putem construi parteneriate de incredere pe baza castigului real si substantial al tuturor."',
+      imgWidth: 307,
+      imgHeight: 84,
+      description: `"Apariția UBEON a fost pentru noi surprinzătoare. UBEON este partenerul de care aveam nevoie și care în timp a dovedit implicare și atenție la nevoile partenerului. Echipa de acolo este consecventă ideii de a aduce soluții inovatoare în deplin acord cu interesele partenerilor săi. Recomand cu sinceritate să începeți o colaborare cu echipa UBEON și să vă convingeți că putem construi parteneriate de încredere pe baza câștigului real și substanțial al tuturor."`,
       author: "Bogdan Negera - Managing Partner",
     },
     {
-      title: "Asociatia Planul B",
+      title: "Asociația Planul B",
       src: "/images/planulb.png",
-      description:
-        '"In anul 2016 UBEON se afla in cautarea unui partener pentru sustinerea proiectelor sale de responsabilitate sociala, iar nou infiintata noastra asociatie cauta solutii organizatorice si logistice la inceput de drum. Agelerea de a deveni parteneri sustenabili pe un astfel de palier social a fost una naturala. De la primele intalniri am descoperit un adevarat partener. Coordonatele au fost usor de armonizat si am pus bazele unui parteneriat durabil care inseamna sustinere si sprijin logistic pentru programele noastre, precum si implicare in organizarea evenimentelor si actiunilor noastre sociale."',
-      author: "Lucian Constantin - Presedinte",
+      imgWidth: 400,
+      imgHeight: 159,
+      description: `"În anul 2016 UBEON se afla în căutarea unui partener pentru susținerea proiectelor sale de responsabilitate socială, iar nou înființata noastră asociație căuta soluții organizatorice și logistice la început de drum. Alegerea de a deveni parteneri sustenabili pe un astfel de palier social a fost una naturală. De la primele întâlniri am descoperit un adevărat partener. Coordonatele au fost ușor de armonizat și am pus bazele unui parteneriat durabil care înseamnă susținere și sprijin logistic pentru programele noastre, precum și implicare în organizarea evenimentelor și acțiunilor noastre sociale."`,
+      author: "Lucian Constantin - Președinte",
     },
   ];
 
@@ -56,19 +61,26 @@ const page = () => {
                   {partner.title}
                 </CardTitle>
                 <CardContent className="p-4">
-                  <div className="flex justify-evenly flex-col lg:flex-row gap-3">
-                    <Image
-                      className="rounded-lg"
-                      src={partner.src}
-                      width={300}
-                      height={100}
-                      alt={partner.title}
-                    />
-                    <p className="max-w-[650px]">{partner.description}</p>
+                  <div className="flex flex-col lg:flex-row items-center gap-6">
+                    <div className="min-w-[150px] flex justify-center">
+                      <Image
+                        src={partner.src}
+                        alt={partner.title}
+                        width={partner.imgWidth}
+                        height={partner.imgHeight}
+                        className="object-contain"
+                        style={{ width: partner.imgWidth, height: partner.imgHeight }}
+                      />
+                    </div>
+                    <p className="text-justify italic text-gray-800 max-w-2xl">
+                      {partner.description}
+                    </p>
                   </div>
                 </CardContent>
                 <CardFooter className="flex justify-end p-4">
-                  <p className="text-sm font-semibold italic text-gray-600">{partner.author}</p>
+                  <p className="text-sm font-semibold italic text-gray-600">
+                    {partner.author}
+                  </p>
                 </CardFooter>
               </Card>
             </div>
